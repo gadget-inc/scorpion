c73d12e16328fba72ad35ce07d73b8e05356cb73
Harry Brundage <harry.brundage@gmail.com>
2019-11-22 10:04:33 -0500

    Use another que worker to limit crawl concurrency

--- app/jobs/crawler/execute_crawl_job.rb
+++ app/jobs/crawler/execute_crawl_job.rb
@@ -3,6 +3,7 @@
 class Crawler::ExecuteCrawlJob < Que::Job
   self.maximum_retry_count = 0
   self.exclusive_execution_lock = true
+  self.queue = "crawls"
 
   def run(property_id:, reason:)
     property = Property.find(property_id)
