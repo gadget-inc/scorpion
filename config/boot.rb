78404442a5a5c032fe9df31d4696b4cf6df29702
Harry Brundage <harry.brundage@gmail.com>
2020-01-17 12:19:25 -0500

    remove debugging bs

--- config/boot.rb
+++ config/boot.rb
@@ -3,8 +3,4 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-
-# Debug an issue on circle where trestle admin names were coming out as mangled strings
-unless ENV["CI"]
-  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-end
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
