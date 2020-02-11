6b908bd47df4ee5315be1fbc2f69f3905610ac8a
Harry Brundage <harry.brundage@gmail.com>
2020-02-11 17:45:58 -0500

    Set up sentry release

--- config/deploy/deploy.sh
+++ config/deploy/deploy.sh
@@ -10,3 +10,11 @@ export REVISION=${REVISION:-$GIT_SHA}
 
 # deploy application to it's namespace
 bundle exec krane render -f config/deploy/$ENVIRONMENT --current-sha=$REVISION | bundle exec krane deploy scorpion-production $CLUSTER --stdin -f config/deploy/$ENVIRONMENT/secrets.ejson
+
+if [-n "$SENTRY_AUTH_TOKEN"]; then
+  sentry-cli releases new -p scorpion-backend -p scorpion-frontend $REVISION
+  sentry-cli releases set-commits --auto $REVISION
+  sentry-cli releases finalize "$REVISION"
+else
+  echo "Not sending release info to Sentry as SENTRY_AUTH_TOKEN is not set"
+fi
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,5 @@ Raven.configure do |config|
   if Rails.env.production?
     config.dsn = ENV["BACKEND_SENTRY_DSN"]
   end
+  config.release = Fixings::AppRelease.current
 end
