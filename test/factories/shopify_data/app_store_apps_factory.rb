246c735b000f5edd9bf2fba8e20bd766d0cae2e0
Harry Brundage <harry.brundage@gmail.com>
2020-04-09 15:11:44 -0400

    Fix factories

--- test/factories/shopify_data/app_store_apps_factory.rb
+++ test/factories/shopify_data/app_store_apps_factory.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 FactoryBot.define do
   factory :shopify_data_app_store_app, class: "ShopifyData::AppStoreApp" do
-    title { "MyString" }
-    app_store_url { "MyString" }
-    app_store_developer_url { "MyString" }
-    developer_name { "MyString" }
-    developer_url { "MyString" }
-    faq_url { "MyString" }
-    category { "MyString" }
-    image_url { "MyString" }
+    title { "Superspeed - Free Speed Boost" }
+    app_store_url { "https://apps.shopify.com/superspeed-free-speed-boost" }
+    app_store_developer_url { "https://apps.shopify.com/partners/scorpion2" }
+    developer_name { "Gadget" }
+    developer_url { "https://superspeed.gadget.dev/" }
+    faq_url { nil }
+    category { "Store design" }
+    image_url { "https://apps.shopifycdn.com/listing_images/2ff65525543e6c40c30a54a999e386c7/icon/5605cd688ee441350dd549eab5abcf18.png?height=168&width=168" }
+    inferred_domains { ["superspeed.gadget.dev"] }
+    confirmed_domains { ["superspeed.gadget.dev"] }
+    priority { 1 }
   end
 end
--- test/factories/shopify_data/detected_apps_factory.rb
+++ test/factories/shopify_data/detected_apps_factory.rb
@@ -7,6 +7,7 @@ FactoryBot.define do
     last_seen_at { "2020-03-16 17:53:59" }
     seen_last_time { true }
     reasons { ["Detected third party javascript"] }
+    subject_key { "third-party-web-Yotpo" }
 
     after(:build) do |app|
       app.account ||= app.shopify_shop.account
